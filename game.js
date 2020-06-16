aa415c91430e931f376d4ad9c4092316b48aec38
sector032 <szantodavid032@gmail.com>
2020-06-16 15:14:11 +0200

    time function, need to improve CSS

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 initGame();
-shuffleCards();
+timeLeft();
+
 
 function initGame() {
 
@@ -72,3 +73,26 @@ function dropZoneDropHandler(event) {
     let draggedElement = document.querySelector('.dragged');
     event.currentTarget.appendChild(draggedElement);
 }
+
+
+function timeLeft(){
+    const timeLeftDisplay = document.querySelector('#time-left')
+    const startBtn = document.querySelector('#start-button')
+    let timeLeft = 10
+
+    function countDown() {
+        setInterval(function () {
+            if(timeLeft <= 0){
+                clearInterval(timeLeft = 0)
+            }
+            timeLeftDisplay.innerHTML = timeLeft
+            timeLeft -=1
+        }, 1000)
+        startBtn.style.visibility = 'hidden'
+    }
+    startBtn.addEventListener('click', countDown)
+    startBtn.addEventListener('click', shuffleCards)
+
+
+}
+
