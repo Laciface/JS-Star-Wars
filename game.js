86c5e355ec02b036eea46404a00cda3c170b27b4
belbanas <belbanas@gmail.com>
2020-06-16 14:41:46 +0200

    Add click rotate

--- game.js
+++ game.js
@@ -1,10 +1,9 @@
 initGame();
-shuffleCards();
 
 function initGame() {
 
     // Your game can start here, but define separate functions, don't write everything in here :)
-
+    shuffleCards();
     initDragAndDrop();
 }
 
@@ -13,13 +12,16 @@ function shuffleCards() {
     for (let i = mixedCardsContainer.children.length; i >= 0; i--) {
         mixedCardsContainer.appendChild(mixedCardsContainer.children[Math.random() * i | 0]);
     }
+    for (let i = 0; i < 9; i++) {
+        mixedCardsContainer.children[i].style.transform = 'rotate(20deg)'
+    }
 }
 
 function initDragAndDrop() {
     let draggables = document.querySelectorAll('.card');
     let dropZones = document.querySelectorAll('.card-slot, .mixed-cards');
     initDraggables(draggables);
-    initDroppables(dropZones);
+    initDropZones(dropZones);
 }
 
 function initDraggables(draggables) {
@@ -28,9 +30,9 @@ function initDraggables(draggables) {
     }
 }
 
-function initDroppables(dropZones) {
+function initDropZones(dropZones) {
     for (let dropZone of dropZones) {
-        initDroppable(dropZone);
+        initDropZone(dropZone);
     }
 }
 
@@ -39,9 +41,11 @@ function initDraggable(draggable) {
     //draggable.addEventListener('drag', dragHandler);
     draggable.addEventListener('dragend', dragEndHandler);
     draggable.setAttribute('draggable', 'true');
+    draggable.addEventListener('contextmenu', clickRotate);
+    //draggable.setAttribute('data-rotate', 0);
 }
 
-function initDroppable(dropZone) {
+function initDropZone(dropZone) {
     //dropZone.addEventListener('dragenter', dropZoneEnterHandler);
     dropZone.addEventListener('dragover', dropZoneOverHandler);
     //dropZone.addEventListener('dragleave', dropZoneLeaveHandler);
@@ -72,5 +76,24 @@ function dropZoneDropHandler(event) {
     let draggedElement = document.querySelector('.dragged');
     if (event.currentTarget.children.length === 0 || event.currentTarget.classList.contains('mixed-cards')) {
         event.currentTarget.appendChild(draggedElement);
+    }
+}
+
+
+function clickRotate(event) {
+    event.preventDefault();
+    let rotateData = parseInt(event.currentTarget.getAttribute('data-rotate'));
+    if (rotateData === 0) {
+        event.currentTarget.style.transform = 'rotate(90deg)';
+        event.currentTarget.setAttribute('data-rotate', '90');
+    } else if (rotateData === 90) {
+        event.currentTarget.style.transform = 'rotate(180deg)';
+        event.currentTarget.setAttribute('data-rotate', '180');
+    } else if (rotateData === 180) {
+        event.currentTarget.style.transform = 'rotate(270deg)';
+        event.currentTarget.setAttribute('data-rotate', '270');
+    } else {
+        event.currentTarget.style.transform = 'rotate(0deg)';
+        event.currentTarget.setAttribute('data-rotate', '0');
     }
 }
