874abba19b4692bda60521922deaba1bf75b598a
belbanas <belbanas@gmail.com>
2020-06-16 11:28:05 +0200

    Add drag and drop function.

--- game.js
+++ game.js
@@ -1,9 +1,11 @@
 initGame();
 shuffleCards();
+
 function initGame() {
 
     // Your game can start here, but define separate functions, don't write everything in here :)
 
+    initDragAndDrop();
 }
 
 function shuffleCards() {
@@ -11,4 +13,62 @@ function shuffleCards() {
     for (let i = mixedCardsContainer.children.length; i >= 0; i--) {
         mixedCardsContainer.appendChild(mixedCardsContainer.children[Math.random() * i | 0]);
     }
+}
+
+function initDragAndDrop() {
+    let draggables = document.querySelectorAll('.card');
+    let dropZones = document.querySelectorAll('.card-slot');
+    initDraggables(draggables);
+    initDroppables(dropZones);
+}
+
+function initDraggables(draggables) {
+    for (let draggable of draggables) {
+        initDraggable(draggable);
+    }
+}
+
+function initDroppables(dropZones) {
+    for (let dropZone of dropZones) {
+        initDroppable(dropZone);
+    }
+}
+
+function initDraggable(draggable) {
+    draggable.addEventListener('dragstart', dragStartHandler);
+    //draggable.addEventListener('drag', dragHandler);
+    draggable.addEventListener('dragend', dragEndHandler);
+    draggable.setAttribute('draggable', 'true');
+}
+
+function initDroppable(dropZone) {
+    dropZone.addEventListener('dragenter', dropZoneEnterHandler);
+    dropZone.addEventListener('dragover', dropZoneOverHandler);
+    dropZone.addEventListener('dragleave', dropZoneLeaveHandler);
+    dropZone.addEventListener('drop', dropZoneDropHandler);
+}
+
+function dragStartHandler() {
+    this.classList.add('dragged');
+}
+
+function dragEndHandler() {
+    this.classList.remove('dragged');
+}
+
+function dropZoneEnterHandler(event) {
+    event.preventDefault();
+}
+
+function dropZoneOverHandler(event) {
+    event.preventDefault();
+}
+
+function dropZoneLeaveHandler(event) {
+    event.preventDefault();
+}
+
+function dropZoneDropHandler(event) {
+    let draggedElement = document.querySelector('.dragged');
+    event.currentTarget.appendChild(draggedElement);
 }
