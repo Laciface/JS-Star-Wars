91ecc5d652aeb1e24fa8348c0e6eb5988943ca82
sector032 <szantodavid032@gmail.com>
2020-06-16 10:37:26 +0200

    shuffle feature

--- game.js
+++ game.js
@@ -1,7 +1,14 @@
 initGame();
-
+shuffleCards();
 function initGame() {
 
     // Your game can start here, but define separate functions, don't write everything in here :)
 
+}
+
+function shuffleCards() {
+    let mixedCardsContainer = document.querySelector(".mixed-cards");
+    for (let i = mixedCardsContainer.children.length; i >= 0; i--) {
+        mixedCardsContainer.appendChild(mixedCardsContainer.children[Math.random() * i | 0]);
+    }
 }
